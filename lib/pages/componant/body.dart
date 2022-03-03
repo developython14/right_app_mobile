9067c67c53cf21c2c07e9ad4da47c62cfd15dee3
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-03 12:09:11 +0000

    start design start app and logging

--- lib/pages/componant/body.dart
+++ lib/pages/componant/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:right_saudi/pages/componant/background.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:right_saudi/pages/componant/rounded_button.dart';
 
 class body extends StatelessWidget {
   const body({Key? key}) : super(key: key);
@@ -17,7 +18,19 @@ class body extends StatelessWidget {
               "welcome to right plateforme",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            SvgPicture('assets/start.jpg')
+            SvgPicture.asset('assets/start.svg'),
+            roundedbutton(
+              text: "login",
+              color: Colors.red,
+              textcolor: Colors.green,
+              press: () {},
+            ),
+            roundedbutton(
+              text: "signup",
+              color: Color.fromARGB(255, 105, 43, 38),
+              textcolor: Color.fromARGB(255, 218, 8, 172),
+              press: () {},
+            )
           ]),
     );
   }
