b21ba3332748722951d2c798f4d1149941fd45b5
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-03 00:55:12 +0000

    push app

--- lib/pages/componant/body.dart
+++ lib/pages/componant/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:right_saudi/pages/componant/background.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class body extends StatelessWidget {
   const body({Key? key}) : super(key: key);
@@ -7,25 +9,16 @@ class body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; //get the size of the device ;
 
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(children: <Widget>[
-        Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/1.png",
-              width: size.width * 0.3,
-            )),
-        Positioned(
-            left: 0,
-            bottom: 0,
-            child: Image.asset(
-              "assets/0.png",
-              width: size.width * 0.2,
-            ))
-      ]),
+    return background(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "welcome to right plateforme",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SvgPicture('assets/start.jpg')
+          ]),
     );
   }
 }
