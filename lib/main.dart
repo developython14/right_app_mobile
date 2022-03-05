90e33f908cc280aac348ee4f12359d8a25addf3f
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-05 03:03:23 +0000

    start design start app and logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:right_saudi/pages/start.dart';
 import 'package:right_saudi/pages/login_screen/login.dart';
+import 'package:right_saudi/test.dart';
 
 void main() {
   runApp(
-      MaterialApp(title: 'mustapha belkassem ', initialRoute: '/mus', routes: {
+      MaterialApp(title: 'mustapha belkassem ', initialRoute: '/test', routes: {
     '/': (context) => const welecome(),
     '/mus': (context) => const login_screen(),
+    '/test': (context) => const MyStatefulWidget(),
   }));
 }
