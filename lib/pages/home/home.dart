a86e6daf29ee04ab61a6cbce3d3418d1952cf8e9
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-08 20:54:51 +0000

    trying every  widgets in flutter

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -7,7 +7,7 @@ class homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 212, 223, 233),
         centerTitle: false,
         title: Padding(
           padding: const EdgeInsets.only(left: 10),
@@ -23,11 +23,21 @@ class homescreen extends StatelessWidget {
           Stack(
             children: [
               Container(
+                margin: EdgeInsets.only(top: 10, right: 20),
+                padding: EdgeInsets.all(8),
                 child: Image.asset(
                   'assets/0.png',
                   width: 30,
                 ),
-              )
+              ),
+              Positioned(
+                  top: 15,
+                  right: 30,
+                  child: Container(
+                      height: 8,
+                      width: 8,
+                      decoration: BoxDecoration(
+                          color: Colors.red, shape: BoxShape.circle)))
             ],
           )
         ],
