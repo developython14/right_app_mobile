bdd51b4c6882de50df5e8d610d3877f378288e50
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-08 22:05:49 +0000

    trying every  widgets in flutter

--- lib/pages/componant/c_home.dart
+++ lib/pages/componant/c_home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color back = Colors.grey;
+const Color front = Colors.white;
+
 class emoji_text extends StatelessWidget {
   const emoji_text({
     Key? key,
@@ -9,6 +12,14 @@ class emoji_text extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: EdgeInsets.only(left: 25),
-        child: Text("MUSTAPHA BELKASSEM EMOJI "));
+        child: RichText(
+          text: TextSpan(children: [
+            TextSpan(
+                text: 'let we do our service',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 26, color: front)),
+            TextSpan(text: '🔥', style: TextStyle(fontSize: 26))
+          ]),
+        ));
   }
 }
