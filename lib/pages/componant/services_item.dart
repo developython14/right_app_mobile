614332223e30c698d08d22ec2ca0e608079dac9e
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-09 22:13:30 +0000

    trying every  widgets in flutter

--- lib/pages/componant/services_item.dart
+++ lib/pages/componant/services_item.dart
@@ -12,9 +12,26 @@ class servicesitem extends StatelessWidget {
         child: Stack(
       children: [
         Container(
-          color: Colors.blue,
-          height: 200,
-          width: 150,
+          height: 250,
+          width: 250,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 172, 255, 174),
+              borderRadius: BorderRadius.circular(25)),
+          child: Column(children: [
+            Expanded(
+              flex: 2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25)),
+                child: Image.asset(
+                  'assets/mustapha.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(flex: 1, child: Text("hello mustapha belkassem"))
+          ]),
         )
       ],
     ));
