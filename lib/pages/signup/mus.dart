42edef5341de7ddfc86b9509e559bd366aa9e808
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-16 23:47:07 +0000

    trying every  widgets in flutter

--- lib/pages/signup/mus.dart
+++ lib/pages/signup/mus.dart
@@ -6,24 +6,38 @@ class mydesign extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; //get the size of the device ;
-    return Stack(
-      children: <Widget>[
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-        ),
-        Container(
-          width: 90,
-          height: 90,
-          color: Colors.green,
-        ),
-        Container(
-          width: 80,
-          height: 80,
-          color: Colors.blue,
-        ),
-      ],
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.all(50.0),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 100,
+            left: 100,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 200,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 0, 224, 120),
+            ),
+          ),
+          Positioned(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 62, 18, 68),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
