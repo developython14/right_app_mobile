613bd8d0f00bedca5b7ff1092fd4c37c1815817c
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-22 22:36:21 +0000

    trying every  widgets in flutter0

--- lib/pages/wait.dart
+++ lib/pages/wait.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-
 class wait extends StatelessWidget {
   const wait({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: Center(child: SpinKitFadingCircle(
+      itemBuilder: (BuildContext context, int index) {
+        return DecoratedBox(
+          decoration: BoxDecoration(
+            color: index.isEven ? Colors.red : Colors.green,
+          ),
+        );
+      },
+    )));
   }
 }
