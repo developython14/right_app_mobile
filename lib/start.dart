181d15da8c8207e201632f0cc06b6e729b44504c
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-22 19:06:02 +0000

    work on intro pages

--- lib/start.dart
+++ lib/start.dart
@@ -2,24 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 class toma extends StatelessWidget {
-  const toma({ Key? key }) : super(key: key);
+  const toma({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    returnchild:IntroductionScreen(
-  pages: listPagesViewModel,
-  done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-  onDone: () {
-    // When done button is press
-  },
-); //Material App Container(
-      child:IntroductionScreen(
-  pages: listPagesViewModel,
-  done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-  onDone: () {
-    // When done button is press
-  },
-); //Material App
+    List listPagesViewModel = [
+      PageViewModel(
+        title: "Title of first page",
+        body:
+            "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.network("https://domaine.com/image.png", height: 175.0),
+        ),
+      ),
+      PageViewModel(
+        title: "Title of first page",
+        body:
+            "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.network("https://domaine.com/image.png", height: 175.0),
+        ),
+      ),
+      PageViewModel(
+        title: "Title of first page",
+        body:
+            "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.network("https://domaine.com/image.png", height: 175.0),
+        ),
+      ),
+      PageViewModel(
+        title: "Title of first page",
+        body:
+            "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.network("https://domaine.com/image.png", height: 175.0),
+        ),
+      )
+    ];
+    return Container(
+      child: IntroductionScreen(
+        pages: [
+          PageViewModel(
+            title: "Title of first page",
+            body:
+                "Here you can write the description of the page, to explain someting...",
+            image: Center(
+              child:
+                  Image.network("https://domaine.com/image.png", height: 175.0),
+            ),
+          ),
+          PageViewModel(
+            title: "Title of first page",
+            body:
+                "Here you can write the description of the page, to explain someting...",
+            image: Center(
+              child:
+                  Image.network("https://domaine.com/image.png", height: 175.0),
+            ),
+          )
+        ],
+        onDone: () {
+          // When done button is press
+        },
+        showSkipButton: true,
+        skip: const Text("Skipm"),
+        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      ),
     );
   }
 }
