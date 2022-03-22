722a43ff92e1231b86f2ac73897bb8d87dc2d00b
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-22 21:27:09 +0000

    trying every  widgets in flutter0

--- lib/start.dart
+++ lib/start.dart
@@ -9,12 +9,19 @@ class toma extends StatelessWidget {
     return SafeArea(
         child: IntroductionScreen(
       pages: [
-        PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
+        PageViewModel(
+            title: "hi mustapha",
+            body: 'toma hook your are big',
+            image: Image.asset('assets/mustapha.jpg')),
         PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
         PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
         PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
       ],
       done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      skip: const Text("skip", style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Text("next", style: TextStyle(fontWeight: FontWeight.w600)),
+      showSkipButton: true,
+      showNextButton: true,
       onDone: () {
         // When done button is press
       },
