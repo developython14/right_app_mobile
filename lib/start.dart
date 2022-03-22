ae808fd226d7874dbcbc9badef143dda4b8c6682
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-22 21:13:56 +0000

    trying every  widgets in flutter0

--- lib/start.dart
+++ lib/start.dart
@@ -6,69 +6,18 @@ class toma extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List listPagesViewModel = [
-      PageViewModel(
-        title: "Title of first page",
-        body:
-            "Here you can write the description of the page, to explain someting...",
-        image: Center(
-          child: Image.network("https://domaine.com/image.png", height: 175.0),
-        ),
-      ),
-      PageViewModel(
-        title: "Title of first page",
-        body:
-            "Here you can write the description of the page, to explain someting...",
-        image: Center(
-          child: Image.network("https://domaine.com/image.png", height: 175.0),
-        ),
-      ),
-      PageViewModel(
-        title: "Title of first page",
-        body:
-            "Here you can write the description of the page, to explain someting...",
-        image: Center(
-          child: Image.network("https://domaine.com/image.png", height: 175.0),
-        ),
-      ),
-      PageViewModel(
-        title: "Title of first page",
-        body:
-            "Here you can write the description of the page, to explain someting...",
-        image: Center(
-          child: Image.network("https://domaine.com/image.png", height: 175.0),
-        ),
-      )
-    ];
-    return Container(
-      child: IntroductionScreen(
-        pages: [
-          PageViewModel(
-            title: "Title of first page",
-            body:
-                "Here you can write the description of the page, to explain someting...",
-            image: Center(
-              child:
-                  Image.network("https://domaine.com/image.png", height: 175.0),
-            ),
-          ),
-          PageViewModel(
-            title: "Title of first page",
-            body:
-                "Here you can write the description of the page, to explain someting...",
-            image: Center(
-              child:
-                  Image.network("https://domaine.com/image.png", height: 175.0),
-            ),
-          )
-        ],
-        onDone: () {
-          // When done button is press
-        },
-        showSkipButton: true,
-        skip: const Text("Skipm"),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
-      ),
-    );
+    return SafeArea(
+        child: IntroductionScreen(
+      pages: [
+        PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
+        PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
+        PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
+        PageViewModel(title: "hi mustapha", body: 'toma hook your are big'),
+      ],
+      done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      onDone: () {
+        // When done button is press
+      },
+    ));
   }
 }
