149e37cdabb9a1533afc693d05151d6928f934f6
djmel1955@gmail.com <djmel1955@gmail.com>
2022-03-24 01:05:56 +0000

    trying every  widgets

--- lib/pages/login_screen/login.dart
+++ lib/pages/login_screen/login.dart
@@ -10,18 +10,37 @@ class login extends StatefulWidget {
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          color: Colors.black,
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(255, 255, 255, 255),
+        padding: EdgeInsets.all(50.0),
+        child: Stack(
+          children: [
+            Container(
+              child: Image.asset("assets/start_app/0.jpg"),
+            ),
+            Positioned(
+              top: 250,
+              child: Text('Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26,
+                  )),
+            ),
+            Positioned(
+              width:200,
+              top:350,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your email',
+                ),
+              ),
+            )
+          ],
         ),
-        Container(
-          color: Colors.white,
-        ),
-        Container(
-          color: Colors.green,
-        )
-      ],
+      ),
     );
   }
 }
